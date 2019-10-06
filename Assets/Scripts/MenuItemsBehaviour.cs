afcd7c8182169238afd7448442ca1425fb0542a0
Leonardo Diegues <dscleo@gmail.com>
2019-10-06 18:11:42 -0300

    Tenta modificar tamanho dos botóes quando passa mouse em cima

---
+++ Assets/Scripts/MenuItemsBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuItemsBehaviour : MonoBehaviour
+{
+    public TextMeshProUGUI itemText;
+    
+    public void OnMouseOver() {
+        print(itemText);
+        itemText.fontSize = itemText.fontSize + 5;
+    }    
+
+    public void OnMouseExit() {
+        print("saiu");
+        itemText.fontSize = itemText.fontSize;
+    }
+}
