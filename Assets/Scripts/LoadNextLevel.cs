659b023491b290388c85875b03189ade67c1e96b
Simon <simon.ducroquet@gmail.com>
2019-10-06 12:04:22 -0300

    passagem de level e prefabs

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D (Collider2D other ) {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
